c97729a6aaf239e398eb0882be4aafd7b7db3d02
k01yasik <bzdykin@mail.ru>
2018-07-30 01:07:27 +0300

    Yandex verification

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,7 +6,7 @@
         <meta name="theme-color" content="#ffffff">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="google-site-verification" content="aLlUmbWjE1uTvbSk_PSrOhYnCMqVjo8DCUvDBuX8n_I" />
-        <meta name='yandex-verification' content='7d1b25881aa614ae' />
+        <meta name="yandex-verification" content="3228411b887596c9" />
         <meta name='wmail-verification' content='0bb02aeb0edea4483300fbf570934ffd' />
         <meta name="msvalidate.01" content="5C7980ED46474154F14D366202946FE3" />
         <meta name="790ac42607dbab4325ac5d1f68751ae2" content="">
