b6c6ab41223e28e1abea3d56fd6ad9d3afb75ba7
k01yasik <bzdykin@mail.ru>
2018-08-11 15:32:57 +0300

    Without source code in js and scss

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.webpackConfig({ devtool: "inline-source-map" });
+//mix.webpackConfig({ devtool: "inline-source-map" });
 
 mix.js('resources/assets/js/app.js', 'public/js')
-    .sass('resources/assets/sass/app.scss', 'public/css').sourceMaps();
+    .sass('resources/assets/sass/app.scss', 'public/css');
