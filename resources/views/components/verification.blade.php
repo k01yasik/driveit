7b1e431c3c3113dd898a769953d1d0652550e0c7
k01yasik <bzdykin@mail.ru>
2018-08-22 01:01:16 +0300

    Robots.txt

---
+++ resources/views/components/verification.blade.php
@@ -0,0 +1,5 @@
+<meta name="google-site-verification" content="aLlUmbWjE1uTvbSk_PSrOhYnCMqVjo8DCUvDBuX8n_I" />
+<meta name="yandex-verification" content="3228411b887596c9" />
+<meta name='wmail-verification' content='0bb02aeb0edea4483300fbf570934ffd' />
+<meta name="msvalidate.01" content="5C7980ED46474154F14D366202946FE3" />
+<meta name="790ac42607dbab4325ac5d1f68751ae2" content="">
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,11 +5,7 @@
         <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
         <meta name="theme-color" content="#ffffff">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="google-site-verification" content="aLlUmbWjE1uTvbSk_PSrOhYnCMqVjo8DCUvDBuX8n_I" />
-        <meta name="yandex-verification" content="3228411b887596c9" />
-        <meta name='wmail-verification' content='0bb02aeb0edea4483300fbf570934ffd' />
-        <meta name="msvalidate.01" content="5C7980ED46474154F14D366202946FE3" />
-        <meta name="790ac42607dbab4325ac5d1f68751ae2" content="">
+        @include('components.verification')
         <meta name="csrf-token" content="{{ csrf_token() }}">
         @include('seo.index')
         <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
