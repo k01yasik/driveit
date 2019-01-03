9f30b7f87ee733c12b5f4f79d7c9756c42e55b93
k01yasik <bzdykin@mail.ru>
2019-01-03 15:11:27 +0300

    Fixing posts links

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             //UserTableSeeder::class,
-            UpdateSeoTableSeeder::class,
             UpdatePostSeeder::class,
         ]);
     }
