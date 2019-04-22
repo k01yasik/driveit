ee0249967d378935e71444aa40fb85bff3f164b2
k01yasik <bzdykin@mail.ru>
2019-04-22 15:43:12 +0300

    Fixing migration 3

--- database/migrations/2019_04_17_190211_create_suggests_table.php
+++ database/migrations/2019_04_17_190211_create_suggests_table.php
@@ -18,7 +18,43 @@ class CreateSuggestsTable extends Migration
             $table->unsignedBigInteger('post_id');
             $table->unsignedBigInteger('suggest');
             $table->timestamps();
+        });
 
+        Schema::table('category_post', function (Blueprint $table) {
+            $table->dropForeign('category_post_post_id_foreign');
+
+            $table->unsignedBigInteger('post_id')->change();
+        });
+
+        Schema::table('comments', function (Blueprint $table) {
+            $table->dropForeign('comments_post_id_foreign');
+
+            $table->unsignedBigInteger('post_id')->change();
+        });
+
+        Schema::table('ratings', function (Blueprint $table) {
+            $table->dropForeign('ratings_post_id_foreign');
+
+            $table->unsignedBigInteger('post_id')->change();
+        });
+
+        Schema::table('posts', function (Blueprint $table) {
+            $table->bigIncrements('id')->change();
+        });
+
+        Schema::table('category_post', function (Blueprint $table) {
+            $table->foreign('post_id')->references('id')->on('posts');
+        });
+
+        Schema::table('comments', function (Blueprint $table) {
+            $table->foreign('post_id')->references('id')->on('posts');
+        });
+
+        Schema::table('ratings', function (Blueprint $table) {
+            $table->foreign('post_id')->references('id')->on('posts');
+        });
+
+        Schema::table('suggests', function (Blueprint $table) {
             $table->foreign('post_id')->references('id')->on('posts');
             $table->foreign('suggest')->references('id')->on('posts');
         });
