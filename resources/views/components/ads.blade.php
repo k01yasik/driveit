5fbee77419abea051f3cb8710c31548d46386211
k01yasik <bzdykin@mail.ru>
2020-01-03 08:30:44 +0300

    convert carousel from jpg to webp

--- resources/views/components/ads.blade.php
+++ resources/views/components/ads.blade.php
@@ -1,6 +1,6 @@
 <div class="leaderboard">
     <div class="leaderboard-text">{{ __('Advertisement') }}</div>
     <div class="advert">
-        <img src="{{ config('filesystems.disks.public.url') }}/Bzdykin/coupon/coupon.jpg" alt="{{ __('Advertisement') }}"/>
+        <img src="{{ config('filesystems.disks.public.url') }}/Bzdykin/coupon/coupon.webp" alt="{{ __('Advertisement') }}"/>
     </div>
 </div>
--- resources/views/components/carousel.blade.php
+++ resources/views/components/carousel.blade.php
@@ -3,25 +3,25 @@
         <ul class="rslides">
             <li>
                 <a href="{{ route('category.show', ['category' => 'auto']) }}">
-                    <img src= "{{ config('filesystems.disks.public.url') }}/Bzdykin/carousel/bmw-m6.jpg" alt="Раздел авто" />
+                    <img src= "{{ config('filesystems.disks.public.url') }}/Bzdykin/carousel/bmw-m6.webp" alt="Раздел авто" />
                 </a>
                 <div class="tooltip-black"><span>Авто</span></div>
             </li>
             <li>
                 <a href="{{ route('category.show', ['category' => 'moto']) }}">
-                    <img src="{{ config('filesystems.disks.public.url') }}/Bzdykin/carousel/moto.jpg" alt="Раздел мото" />
+                    <img src="{{ config('filesystems.disks.public.url') }}/Bzdykin/carousel/moto.webp" alt="Раздел мото" />
                 </a>
                 <div class="tooltip-black"><span>Мото</span></div>
             </li>
             <li>
                 <a href="{{ route('category.show', ['category' => 'law']) }}">
-                    <img src="{{ config('filesystems.disks.public.url') }}/Bzdykin/carousel/law.jpg" alt="Раздел право"/>
+                    <img src="{{ config('filesystems.disks.public.url') }}/Bzdykin/carousel/law.webp" alt="Раздел право"/>
                 </a>
                 <div class="tooltip"><span>Право</span></div>
             </li>
             <li>
                 <a href="{{ route('category.show', ['category' => 'helpful']) }}">
-                    <img src="{{ config('filesystems.disks.public.url') }}/Bzdykin/carousel/helpful.jpg" alt="Раздел полезное"/>
+                    <img src="{{ config('filesystems.disks.public.url') }}/Bzdykin/carousel/helpful.webp" alt="Раздел полезное"/>
                 </a>
                 <div class="tooltip-black"><span>Полезное</span></div>
             </li>
