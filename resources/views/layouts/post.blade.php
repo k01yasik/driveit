b384f9a3bc3a252d9f2569b84499b927a93665e7
k01yasik <bzdykin@mail.ru>
2020-02-16 01:48:30 +0300

    Adding Yandex Metrika to Google Tag Manager

--- resources/views/layouts/post.blade.php
+++ resources/views/layouts/post.blade.php
@@ -37,18 +37,6 @@
         gtag('js', new Date());
         gtag('config', 'UA-149570604-1');
     </script>
-    <script type="text/javascript" >
-        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-            m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-        ym(55662706, "init", {
-            clickmap:true,
-            trackLinks:true,
-            accurateTrackBounce:true,
-            webvisor:true
-        });
-    </script>
     @include('components.schema')
     <script type="application/ld+json">
         {
