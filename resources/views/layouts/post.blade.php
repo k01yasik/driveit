b94f9f02910f958d4e34f9f7c9ea18ab1a07a24b
k01yasik <bzdykin@mail.ru>
2020-02-16 02:36:53 +0300

    Fixing Google Tag Manager Config

--- resources/views/layouts/post.blade.php
+++ resources/views/layouts/post.blade.php
@@ -37,6 +37,12 @@
         gtag('js', new Date());
         gtag('config', 'UA-149570604-1');
     </script>
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-KRG3CV2');
+    </script>
     @include('components.schema')
     <script type="application/ld+json">
         {
@@ -54,6 +60,8 @@
     </script>
 </head>
 <body>
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KRG3CV2"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <noscript><div><img src="https://mc.yandex.ru/watch/55662706" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
     <header>
         @include('components.sitetop')
