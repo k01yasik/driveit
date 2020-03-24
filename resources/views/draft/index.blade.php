f1651181a47ecbc1024150d36ac4be43fc270347
k01yasik <bzdykin@mail.ru>
2020-03-25 00:35:52 +0300

    Adding modularscale functionality

--- resources/views/draft/index.blade.php
+++ resources/views/draft/index.blade.php
@@ -7,7 +7,27 @@
         </div>
         <div class="col s12 m12 l9">
             <div class="user-profile-content">
-                <div>test draft index page</div>
+                <h1>Tortor dui sapien vel sollicitudin lacinia vestibulum arcu.</h1>
+                <p>
+                    Tortor dui sapien vel sollicitudin lacinia vestibulum arcu. Eu aliquam, pellentesque commodo? Montes turpis fusce risus libero viverra netus facilisis neque sapien. Porta habitant imperdiet vitae penatibus. Eu placerat aliquet suscipit dictum natoque amet, gravida nec turpis litora. Justo aptent viverra eu tincidunt? Pharetra dictum fermentum sociosqu vivamus taciti turpis. Risus habitant rhoncus placerat dis tempor sodales metus natoque accumsan dictum? Tristique proin dui, euismod ultrices elit parturient leo posuere. Curabitur elit cum mollis ridiculus augue egestas nascetur phasellus est! Cursus platea, non magna! Purus pellentesque dapibus aliquet curabitur torquent! Volutpat dui.
+                </p>
+                <h2>Aptent bibendum tortor ultricies nec elit.</h2>
+                <p>
+                    Aptent bibendum tortor ultricies nec elit. Porttitor cum faucibus maecenas posuere mus nisi venenatis leo. Sed lectus litora magnis aliquam tristique a. Auctor, tincidunt faucibus pulvinar conubia primis nullam placerat adipiscing ornare nunc. Nunc pellentesque, in lectus scelerisque! Adipiscing venenatis torquent elementum accumsan ornare. Parturient in senectus nam nisi torquent mattis consequat. Commodo fames tristique in! Quam consectetur proin vehicula. Conubia montes placerat, vivamus fringilla dignissim pharetra. Augue himenaeos facilisi proin felis nulla pellentesque. Ullamcorper ligula augue semper interdum taciti.
+                </p>
+                <h3>Molestie, nec rutrum nisi curabitur conubia.</h3>
+                <p>
+                    Molestie, nec rutrum nisi curabitur conubia. Luctus cum suscipit dui porttitor eros torquent inceptos fringilla venenatis. Dictumst malesuada molestie vulputate vestibulum ante vestibulum? Consectetur ridiculus sagittis egestas dictumst porta morbi aenean, primis aptent. Nibh id id ut dis. Parturient aptent dis vulputate rhoncus magna consequat penatibus eu morbi nisl tempus fermentum! Consectetur odio lacus ligula non nisi leo.
+                </p>
+                <h4>Placerat nulla euismod ullamcorper sociis volutpat proin tempor velit tincidunt primis cras praesent.</h4>
+                <p>
+                    Placerat nulla euismod ullamcorper sociis volutpat proin tempor velit tincidunt primis cras praesent. Nulla dignissim, aenean at id augue et pellentesque fusce dignissim dapibus. Vulputate auctor facilisis praesent! Eros sodales, cubilia conubia eleifend ac justo tristique? Aenean dolor nascetur fusce vitae vehicula ultrices leo ultricies. Amet auctor semper est? Rutrum luctus dis primis. Quam nunc egestas nibh nec aenean. Aenean ultricies nullam curabitur a dapibus mi lectus adipiscing vestibulum dolor.
+                </p>
+                <h5>Nunc penatibus nam curabitur.</h5>
+                <h6>Est litora cubilia, donec imperdiet accumsan bibendum consequat magnis nascetur integer</h6>
+                <p>
+                    Nunc penatibus nam curabitur. Est litora cubilia, donec imperdiet accumsan bibendum consequat magnis nascetur integer. Mollis amet fames dolor quam erat sit tortor platea porta. Tincidunt pretium aenean ornare duis dapibus purus vitae. Primis elementum cum imperdiet eleifend conubia quisque etiam sagittis ullamcorper tellus ridiculus nullam. Luctus congue imperdiet himenaeos mauris penatibus quam vivamus odio eros est vulputate metus. Magnis nisl augue potenti dolor mattis habitant faucibus eleifend mauris pretium. Venenatis aliquam id mi torquent, tristique non. Curae; pellentesque quisque nullam.
+                </p>
             </div>
         </div>
     </div>
