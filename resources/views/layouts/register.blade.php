bcdbb7b71201b7b70ccbfdc86c44a2d3f2a779ae
k01yasik <bzdykin@mail.ru>
2020-10-06 20:03:24 +0300

    Trying fixing google recaptca v3

--- resources/views/layouts/register.blade.php
+++ resources/views/layouts/register.blade.php
@@ -71,7 +71,7 @@
     <script src="https://www.google.com/recaptcha/api.js?render={{ config('recaptcha.public_key') }}"></script>
     <script>
         grecaptcha.ready(function() {
-            grecaptcha.execute('{{ config('recaptcha.public_key') }}').then(function(token) {
+            grecaptcha.execute('{{ config('recaptcha.public_key') }}', {action: 'submit'}).then(function(token) {
                 document.getElementById("recaptcha_token").value = token;
             });
         });
