8fbd2f3a19cbbcbd94f4ba6ff49533ea6d479c1a
k01yasik <bzdykin@mail.ru>
2020-10-07 03:15:40 +0300

    Fixing displaying errors in registration form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,39 +10,39 @@
                 @csrf
                 <input type="hidden" name="recaptcha_token" id="recaptcha_token">
 
-                @if ($errors->has('username'))
-                    <span role="alert" class="error-message">{{ $errors->first('username') }}</span>
-                @endif
-
                 <label for="username">{{ __('Username') }}</label>
 
                 <input id="username" type="text" class="{{ $errors->has('username') ? 'is-invalid' : '' }}" name="username" value="{{ old('username') }}" required autofocus>
 
-                @if ($errors->has('email'))
-                    <span role="alert" class="error-message">{{ $errors->first('email') }}</span>
+                @if ($errors->has('username'))
+                    <span role="alert" class="error-message">{{ $errors->first('username') }}</span>
                 @endif
 
                 <label for="email">{{ __('E-Mail Address') }}</label>
 
                 <input id="email" type="email" class="{{ $errors->has('email') ? 'is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
 
-                @if ($errors->has('password'))
-                    <span role="alert" class="error-message">{{ $errors->first('password') }}</span>
+                @if ($errors->has('email'))
+                    <span role="alert" class="error-message">{{ $errors->first('email') }}</span>
                 @endif
 
                 <label for="password">{{ __('Password') }}</label>
 
                 <input id="password" type="password" class="{{ $errors->has('password') ? 'is-invalid' : '' }}" name="password" required>
 
+                @if ($errors->has('password'))
+                    <span role="alert" class="error-message">{{ $errors->first('password') }}</span>
+                @endif
+
                 <label for="password-confirm">{{ __('Confirm Password') }}</label>
 
                 <input id="password-confirm" type="password" name="password_confirmation" required>
 
+                <button type="submit" class="button submit-form margin-top-3">{{ __('Register') }}</button>
+
                 @if ($errors->has('recaptcha'))
                     <span role="alert" class="error-message">{{ $errors->first('recaptcha') }}</span>
                 @endif
-
-                <button type="submit" class="button submit-form margin-top-3">{{ __('Register') }}</button>
             </form>
             <p class="privacy-policy">
                 This site is protected by reCAPTCHA and the Google <a href="https://policies.google.com/privacy">Privacy Policy</a>
