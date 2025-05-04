3f4c3b29dbce830549796fd8bca19b3ed094822e
Nickolay Smirnov <bzdykin@mail.ru>
2025-05-04 21:06:13 +0300

    Update 2018_10_17_214834_create_favorites_table.php

--- database/migrations/2018_10_17_214834_create_favorites_table.php
+++ database/migrations/2018_10_17_214834_create_favorites_table.php
@@ -15,7 +15,7 @@ class CreateFavoritesTable extends Migration
     {
         Schema::create('favorites', function (Blueprint $table) {
             $table->id();
-            $table->foregnId('user_id');
+            $table->foreignId('user_id');
             $table->foreignId('image_id');
             $table->timestamps();
 
