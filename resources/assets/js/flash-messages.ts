114780217e5058bdfce051e373f4a834c4959e89
Nickolay Smirnov <bzdykin@mail.ru>
2025-05-08 04:49:25 +0300

    Update flash-messages.ts

--- resources/assets/js/flash-messages.ts
+++ resources/assets/js/flash-messages.ts
@@ -1,1 +1,9 @@
-$('.alert').fadeOut(5000);
+const alerts = document.querySelectorAll<HTMLElement>('.alert');
+alerts.forEach(alert => {
+    // Плавное исчезновение (аналог fadeOut)
+    alert.style.transition = 'opacity 0.5s';
+    alert.style.opacity = '0';
+    
+    // Удаление из DOM после анимации
+    setTimeout(() => alert.remove(), 5000);
+});
