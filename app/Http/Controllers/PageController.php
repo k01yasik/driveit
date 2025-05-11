de6c7b1858c5f29784adde082ced1bac716a3353
Nickolay Smirnov <bzdykin@mail.ru>
2025-05-11 15:46:13 +0300

    Update PageController.php

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -1,297 +1,259 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers;
 
-use App\Services\PaginatorService;
-use App\Services\PostService;
-use App\Services\SuggestsService;
-use Illuminate\Http\Request;
-use App\Services\SeoService;
-use App\Services\CommentService;
-use App\Services\PaginateService;
-use App\Services\PostSortService;
-use Illuminate\Support\Facades\Auth;
+use App\Services\{
+    PostService,
+    SeoService,
+    CommentService,
+    SuggestsService,
+    PaginatorService,
+    PostSortService
+};
 use App\Services\SortService\Interfaces\SortFactory;
+use App\DTO\CommentDTO;
+use Illuminate\Http\Request;
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Auth;
 
 class PageController extends Controller
 {
     public function __construct(
         private SeoService $seoService,
         private CommentService $commentService,
-        private PaginateService $paginateService,
-        private PostSortService $postSortService,
         private PostService $postService,
         private PaginatorService $paginatorService,
         private SuggestsService $suggestsService,
-        private SortFactory $sortFactory
-    )
-    {}
+        private SortFactory $sortFactory,
+        private PostSortService $postSortService
+    ) {}
 
-    public function home(Request $request): \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Contracts\Foundation\Application
+    public function home(Request $request): View
     {
-        $seo = $this->seoService->getSeoData($request);
-
-        $posts = $this->postService->getTopPosts(config('pagination.postsPerPage'));
-
-        return view('page.home', compact('seo', 'posts'));
+        return view('page.home', [
+            'seo' => $this->seoService->getSeoData($request),
+            'posts' => $this->postService->getTopPosts(config('pagination.postsPerPage'))
+        ]);
     }
 
-    public function index(Request $request): \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Contracts\Foundation\Application
+    public function index(Request $request): View
     {
-        $seo = $this->seoService->getSeoData($request);
-
         $posts = $this->postService->getPostsForPage(1, config('pagination.postsPerPage'));
+        $posts = $this->postService->calculateAndPaginate($posts, 1);
 
-        $posts = $this->postService->calculatePostStats($posts);
-
-        $posts = $this->postService->getPostsPaginator($posts, 1, config('pagination.postsPerPage'));
-
-        list($previousNumberPage, $nextNumberPage, $lastNumberPage) = $this->paginatorService->calculatePages($posts);
-
-        return view('posts.index', compact('seo', 'posts', 'nextNumberPage', 'previousNumberPage', 'lastNumberPage'));
+        return view('posts.index', array_merge(
+            ['seo' => $this->seoService->getSeoData($request)],
+            $this->getPaginationData($posts)
+        ));
     }
 
-    public function paginate($id, Request $request): \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Contracts\Foundation\Application
+    public function paginate(int $id, Request $request): View
     {
-        $seo = $this->seoService->getSeoData($request);
-
         $posts = $this->postService->getPostsForPage($id, config('pagination.postsPerPage'));
+        $posts = $this->postService->calculateAndPaginate($posts, $id);
 
-        $posts = $this->postService->calculatePostStats($posts);
-
-        $posts = $this->postService->getPostsPaginator($posts, $id, config('pagination.postsPerPage'));
-
-        list($previousNumberPage, $nextNumberPage, $lastNumberPage) = $this->paginatorService->calculatePages($posts);
-
-        return view('posts.index', compact('seo', 'posts', 'nextNumberPage', 'previousNumberPage', 'lastNumberPage'));
+        return view('posts.index', array_merge(
+            ['seo' => $this->seoService->getSeoData($request)],
+            $this->getPaginationData($posts)
+        ));
     }
 
-    public function bestRated(Request $request): \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Contracts\Foundation\Application
+    public function bestRated(Request $request): View
     {
-        $seo = $this->seoService->getSeoData($request);
-
-        $url= url()->current();
-
-        $breadcrumb = __('Best posts by rating');
-
-        $posts = $this->postService->getAllPosts();
-
-        $posts = $this->postService->calculatePostStats($posts);
-
+        $posts = $this->postService->getAllPostsWithStats();
         $posts = $this->sortFactory->createPostSortByRating()->sort($posts);
+        $posts = $this->postService->paginatePosts(array_slice($posts, 0, config('pagination.postsPerPage')), 1);
 
-        $posts = array_slice($posts, 0, config('pagination.postsPerPage'));
-
-        $posts = $this->postService->getPostsPaginator($posts, 1, config('pagination.postsPerPage'));
-
-        list($previousNumberPage, $nextNumberPage, $lastNumberPage) = $this->paginatorService->calculatePages($posts);
-
-        return view('posts.list', compact('seo', 'posts', 'url', 'nextNumberPage', 'previousNumberPage', 'lastNumberPage', 'breadcrumb'));
+        return $this->renderSortedPostsView(
+            $request,
+            $posts,
+            __('Best posts by rating')
+        );
     }
 
-    public function bestComments(Request $request): \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Contracts\Foundation\Application
+    public function bestComments(Request $request): View
     {
-        $seo = $this->seoService->getSeoData($request);
-
-        $url= url()->current();
-
-        $breadcrumb = __('Best posts by comments');
-
-        $posts = $this->postService->getAllPosts();
-
-        $posts = $this->postService->calculatePostStats($posts);
-
+        $posts = $this->postService->getAllPostsWithStats();
         $posts = $this->sortFactory->createPostSortByComments()->sort($posts);
+        $posts = $this->postService->paginatePosts(array_slice($posts, 0, config('pagination.postsPerPage')), 1);
 
-        $posts = array_slice($posts, 0, config('pagination.postsPerPage'));
-
-        $posts = $this->postService->getPostsPaginator($posts, 1, config('pagination.postsPerPage'));
-
-        list($previousNumberPage, $nextNumberPage, $lastNumberPage) = $this->paginatorService->calculatePages($posts);
-
-        return view('posts.list', compact('seo', 'posts', 'url', 'nextNumberPage', 'previousNumberPage', 'lastNumberPage', 'breadcrumb'));
+        return $this->renderSortedPostsView(
+            $request,
+            $posts,
+            __('Best posts by comments')
+        );
     }
 
-    public function bestCommentsByMonth(Request $request): \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Contracts\Foundation\Application
+    public function bestCommentsByMonth(Request $request): View
     {
-        $seo = $this->seoService->getSeoData($request);
-
-        $breadcrumb = __('Best of the month by comments');
-
-        $posts = $this->postService->getPostsByMonth();
-
-        $posts = $this->postService->calculatePostStats($posts);
-
-        $posts = $this->sortFactory->createPostSortByComments()->sort($posts);
-
-        return view('posts.best-comments', compact('seo', 'posts', 'breadcrumb'));
+        return view('posts.best-comments', [
+            'seo' => $this->seoService->getSeoData($request),
+            'posts' => $this->postSortService->getMonthlyTopByComments(),
+            'breadcrumb' => __('Best of the month by comments')
+        ]);
     }
 
-    public function bestViews(Request $request): \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Contracts\Foundation\Application
+    public function bestViews(Request $request): View
     {
-        $seo = $this->seoService->getSeoData($request);
-
-        $url= url()->current();
-
-        $breadcrumb = __('Best posts by views');
-
         $posts = $this->postService->getPostsSortedByViews(1, config('pagination.postsPerPage'));
+        $posts = $this->postService->calculateAndPaginate($posts, 1);
 
-        $posts = $this->postService->calculatePostStats($posts);
-
-        $posts = $this->postService->getPostsPaginator($posts, 1, config('pagination.postsPerPage'));
-
-        list($previousNumberPage, $nextNumberPage, $lastNumberPage) = $this->paginatorService->calculatePages($posts);
-
-        return view('posts.list', compact('seo', 'posts', 'url', 'nextNumberPage', 'previousNumberPage', 'lastNumberPage', 'breadcrumb'));
+        return $this->renderSortedPostsView(
+            $request,
+            $posts,
+            __('Best posts by views')
+        );
     }
 
-    public function commentsPaginate($id, Request $request): \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Contracts\Foundation\Application
+    public function commentsPaginate(int $id, Request $request): View
     {
-        $breadcrumb = __('Best posts by comments');
-
-        list($postsPerPage, $offset, $seo, $url, $posts) = $this->prepareData($id, $request);
-
-        $posts = $this->sortFactory->createPostSortByComments()->sort($posts);
-
-        $posts = array_slice($posts, $offset, $postsPerPage);
-
-        $posts = $this->postService->getPostsPaginator($posts, $id, $postsPerPage);
-
-        list($previousNumberPage, $nextNumberPage, $lastNumberPage) = $this->paginatorService->calculatePages($posts);
-
-        return view('posts.list', compact('seo', 'posts', 'url', 'nextNumberPage', 'previousNumberPage', 'lastNumberPage', 'breadcrumb'));
+        return $this->paginateSortedPosts(
+            $id,
+            $request,
+            'createPostSortByComments',
+            __('Best posts by comments')
+        );
     }
 
-    public function ratedPaginate($id, Request $request): \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Contracts\Foundation\Application
+    public function ratedPaginate(int $id, Request $request): View
     {
-        $breadcrumb = __('Best posts by rating');
-
-        list($postsPerPage, $offset, $seo, $url, $posts) = $this->prepareData($id, $request);
-
-        $posts = $this->sortFactory->createPostSortByRating()->sort($posts);
-
-        $posts = array_slice($posts, $offset, $postsPerPage);
-
-        $posts = $this->postService->getPostsPaginator($posts, $id, $postsPerPage);
-
-        list($previousNumberPage, $nextNumberPage, $lastNumberPage) = $this->paginatorService->calculatePages($posts);
-
-        return view('posts.list', compact('seo', 'posts', 'url', 'nextNumberPage', 'previousNumberPage', 'lastNumberPage', 'breadcrumb'));
+        return $this->paginateSortedPosts(
+            $id,
+            $request,
+            'createPostSortByRating',
+            __('Best posts by rating')
+        );
     }
 
-    public function viewsPaginate($id, Request $request): \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Contracts\Foundation\Application
+    public function viewsPaginate(int $id, Request $request): View
     {
-        $breadcrumb = __('Best posts by views');
+        $posts = $this->postService->getPostsSortedByViews($id, config('pagination.postsPerPage'));
+        $posts = $this->postService->calculateAndPaginate($posts, $id);
 
-        $postsPerPage = config('pagination.postsPerPage');
-
-        $seo = $this->getSeoData($request, $id);
-
-        $url = $this->getUrlBreadcrumb($request);
-
-        $posts = $this->postService->getPostsSortedByViews($id, $postsPerPage);
-
-        $posts = $this->postService->calculatePostStats($posts);
-
-        $posts = $this->postService->getPostsPaginator($posts, $id, $postsPerPage);
-
-        list($previousNumberPage, $nextNumberPage, $lastNumberPage) = $this->paginatorService->calculatePages($posts);
-
-        return view('posts.list', compact('seo', 'posts', 'url', 'nextNumberPage', 'previousNumberPage', 'lastNumberPage', 'breadcrumb'));
+        return $this->renderSortedPostsView(
+            $request,
+            $posts,
+            __('Best posts by views')
+        );
     }
 
-    public function about(Request $request): \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Contracts\Foundation\Application
+    public function about(Request $request): View
     {
-        $seo = $this->seoService->getSeoData($request);
-
-        return view('page.about', compact('seo'));
+        return view('page.about', [
+            'seo' => $this->seoService->getSeoData($request)
+        ]);
     }
 
-    public function rules(Request $request): \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Contracts\Foundation\Application
+    public function rules(Request $request): View
     {
-        $seo = $this->seoService->getSeoData($request);
-
-        return view('page.rules', compact('seo'));
+        return view('page.rules', [
+            'seo' => $this->seoService->getSeoData($request)
+        ]);
     }
 
-    public function show($slug): \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Contracts\Foundation\Application
+    public function show(string $slug): View
     {
-        $post = $this->postService->getPostsForShow($slug);
-
-        $post = $this->postService->calculatePostStats($post);
-
-        $post = $post[0];
-
-        $suggestIds = $this->suggestsService->getSuggestsIds($post);
-
-        $suggestPosts = $this->postService->getSuggests($suggestIds);
-
-        $suggestPosts = $this->postService->calculatePostStats($suggestPosts);
-
+        $post = $this->postService->getPostWithStats($slug);
         $this->postService->incrementViews($post['id']);
 
-        $seo = [
+        return view('posts.show', [
+            'seo' => $this->getPostSeoData($post),
+            'post' => $post,
+            'sortedComments' => $this->commentService->getNestedComments($post['id']),
+            'authenticated' => Auth::check(),
+            'suggestPosts' => $this->getSuggestedPosts($post)
+        ]);
+    }
+
+    public function notFound(Request $request): View
+    {
+        return view('errors.404');
+    }
+
+    public function storeComment(Request $request): array
+    {
+        $validated = $request->validate([
+            'post' => 'required|integer',
+            'message' => 'required|string|max:5000',
+            'level' => 'required|integer',
+            'parent' => 'nullable|integer'
+        ]);
+
+        $commentDTO = new CommentDTO(
+            postId: (int)$validated['post'],
+            message: (string)$validated['message'],
+            level: (int)$validated['level'],
+            parentId: isset($validated['parent']) ? (int)$validated['parent'] : null
+        );
+
+        return $this->commentService->store($commentDTO)->toArray();
+    }
+
+    private function getSuggestedPosts(array $post): array
+    {
+        $suggestIds = $this->suggestsService->getSuggestsIds($post);
+        return $this->postService->getSuggestsWithStats($suggestIds);
+    }
+
+    private function getPostSeoData(array $post): array
+    {
+        return [
             "title" => $post['title'],
             "description" => $post['description'],
             "image" => $post['image_path'],
             "type" => "article"
         ];
-
-        $authenticated = Auth::check();
-
-        $sortedComments = $this->commentService->sortComments($post['id']);
-
-        return view('posts.show', compact('seo', 'post', 'sortedComments', 'authenticated', 'suggestPosts'));
     }
 
-    public function notFound(Request $request): \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Contracts\Foundation\Application
-    {
-        return view('errors.404');
-    }
-
-    /**
-     * @param Request $request
-     * @param $id
-     * @return array
-     */
-    private function getSeoData(Request $request, $id): array
-    {
-        $seo = $this->seoService->getSeoData($request);
-
-        $seo['title'] = $seo['title'] . '. Страница - ' . $id . '.';
-        $seo['description'] = $seo['description'] . '. Страница - ' . $id . '.';
-        return $seo;
-    }
-
-    /**
-     * @param Request $request
-     * @return string
-     */
-    private function getUrlBreadcrumb(Request $request): string
-    {
-        return url('/') . '/' . $request->segment(1);
-    }
-
-    /**
-     * @param $id
-     * @param Request $request
-     * @return array
-     */
-    private function prepareData($id, Request $request): array
-    {
+    private function paginateSortedPosts(
+        int $id,
+        Request $request,
+        string $sortMethod,
+        string $breadcrumb
+    ): View {
         $postsPerPage = config('pagination.postsPerPage');
-
         $offset = ($id - 1) * $postsPerPage;
 
-        $seo = $this->getSeoData($request, $id);
+        $posts = $this->postService->getAllPostsWithStats();
+        $posts = $this->sortFactory->$sortMethod()->sort($posts);
+        $posts = $this->postService->paginatePosts(
+            array_slice($posts, $offset, $postsPerPage),
+            $id
+        );
 
-        $url = $this->getUrlBreadcrumb($request);
+        return $this->renderSortedPostsView(
+            $request,
+            $posts,
+            $breadcrumb
+        );
+    }
 
-        $posts = $this->postService->getAllPosts();
+    private function renderSortedPostsView(
+        Request $request,
+        array $posts,
+        string $breadcrumb
+    ): View {
+        return view('posts.list', array_merge(
+            [
+                'seo' => $this->seoService->getSeoData($request),
+                'url' => url()->current(),
+                'breadcrumb' => $breadcrumb
+            ],
+            $this->getPaginationData($posts)
+        ));
+    }
 
-        $posts = $this->postService->calculatePostStats($posts);
-
-        return array($postsPerPage, $offset, $seo, $url, $posts);
+    private function getPaginationData(array $posts): array
+    {
+        [$previous, $next, $last] = $this->paginatorService->calculatePages($posts);
+        
+        return [
+            'posts' => $posts,
+            'previousNumberPage' => $previous,
+            'nextNumberPage' => $next,
+            'lastNumberPage' => $last
+        ];
     }
 }
