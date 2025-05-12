d5d79eb192b6f9ce4c162e54c516988949efd9d2
Nickolay Smirnov <bzdykin@mail.ru>
2025-05-12 16:46:26 +0300

    Update AlbumRepository.php

--- app/Repositories/AlbumRepository.php
+++ app/Repositories/AlbumRepository.php
@@ -4,27 +4,33 @@ namespace App\Repositories;
 
 use App\Repositories\Interfaces\AlbumRepositoryInterface;
 use App\Album;
+use App\Dto\AlbumDTO;
 use Illuminate\Support\Str;
 
 class AlbumRepository implements AlbumRepositoryInterface
 {
-
-    public function getUserAlbumByName(string $albumName, int $id): array
+    public function getUserAlbumByName(string $albumName, int $userId): ?Album
     {
-        return Album::where([['user_id', $id], ['name', $albumName]])->first()->toArray();
+        return Album::where([
+            ['user_id', $userId],
+            ['name', $albumName]
+        ])->first();
     }
 
-    public function save(string $albumName, int $userId): void
+    public function save(AlbumDTO $albumDTO): Album
     {
-        $album = new Album;
-        $album->name = $albumName;
-        $album->path = Str::random(10);
-        $album->user_id = $userId;
-        $album->save();
+        return Album::create([
+            'name' => $albumDTO->name,
+            'path' => Str::random(10),
+            'user_id' => $albumDTO->userId
+        ]);
     }
 
     public function updateName(string $oldAlbumName, string $newAlbumName, int $userId): void
     {
-        Album::where([['name', $oldAlbumName], ['user_id', $userId]])->update(['name' => $newAlbumName]);
+        Album::where([
+            ['name', $oldAlbumName],
+            ['user_id', $userId]
+        ])->update(['name' => $newAlbumName]);
     }
 }
