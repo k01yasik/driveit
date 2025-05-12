8a0f743b0888920d16c16f784d8c475af6e36cbb
Nickolay Smirnov <bzdykin@mail.ru>
2025-05-12 20:56:51 +0300

    Update AdminCommentService.php

--- app/Services/AdminCommentService.php
+++ app/Services/AdminCommentService.php
@@ -1,36 +1,39 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Services;
 
-
-use App\Repositories\CachedCommentRepository;
+use App\Repositories\Interfaces\CommentRepositoryInterface;
+use App\DTO\CommentUpdateDTO;
 
 class AdminCommentService
 {
-    protected CachedCommentRepository $commentRepository;
+    public function __construct(
+        private CommentRepositoryInterface $commentRepository
+    ) {}
 
-    public function __construct(CachedCommentRepository $commentRepository)
-    {
-        $this->commentRepository = $commentRepository;
-    }
-
-    public function publish($id): void
+    public function publish(int $id): void
     {
         $comment = $this->commentRepository->getById($id);
-
-        $comment = $this->changeVerifyComment($comment);
-
-        $this->commentRepository->update($comment);
+        $updatedComment = $this->toggleVerificationStatus($comment);
+        
+        $this->commentRepository->update(
+            new CommentUpdateDTO(
+                id: $id,
+                userId: $comment['user_id'],
+                postId: $comment['post_id'],
+                message: $comment['message'],
+                level: $comment['level'],
+                parentId: $comment['parent_id'],
+                isVerified: !$comment['is_verified']
+            )
+        );
     }
 
-    private function changeVerifyComment(array $comment)
+    private function toggleVerificationStatus(array $comment): array
     {
-        if ($comment['is_verified']) {
-            $comment['is_verified'] = 0;
-        } else {
-            $comment['is_verified'] = 1;
-        }
-
+        $comment['is_verified'] = !$comment['is_verified'];
         return $comment;
     }
 }
