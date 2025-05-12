fe949c55eef7e060ac34e9b26fa79edc66d7eb08
Nickolay Smirnov <bzdykin@mail.ru>
2025-05-12 17:59:03 +0300

    Create Thumbnail.php

---
+++ app/Services/Thumbnail.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Services\Thumbnail;
+
+use Intervention\Image\ImageManager;
+
+class ThumbnailGenerator
+{
+    public function __construct(
+        private ImageManager $imageManager
+    ) {}
+
+    public function generate(UploadedFile $file, array $cropData = null): string
+    {
+        $image = $this->imageManager->make($file);
+        
+        if ($cropData) {
+            $image->crop(
+                $cropData['width'],
+                $cropData['height'],
+                $cropData['x'],
+                $cropData['y']
+            );
+        }
+        
+        return $image->encode('jpg', 80)->stream();
+    }
+}
