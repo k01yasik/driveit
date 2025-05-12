9ffc0f4178fe813d8c0523d7e7ac1b8c6841b1c9
Nickolay Smirnov <bzdykin@mail.ru>
2025-05-12 17:49:33 +0300

    Update AvatarImageRequest.php

--- app/Http/Requests/AvatarImageRequest.php
+++ app/Http/Requests/AvatarImageRequest.php
@@ -6,6 +6,11 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class AvatarImageRequest extends FormRequest
 {
+    public function authorize()
+    {
+        return true;
+    }
+    
     public function rules(): array
     {
         return [
