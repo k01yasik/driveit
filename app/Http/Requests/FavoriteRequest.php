c3ee147d89295ba24fbaa400792f9dc8dae3d28f
Nickolay Smirnov <bzdykin@mail.ru>
2025-05-14 04:54:11 +0300

    Update FavoriteRequest.php

--- app/Http/Requests/FavoriteRequest.php
+++ app/Http/Requests/FavoriteRequest.php
@@ -3,29 +3,55 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 
 class FavoriteRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
-     *
+     * 
      * @return bool
      */
-    public function authorize()
+    public function authorize(): bool
     {
-        return true;
+        return Auth::check();
     }
 
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array
+     * @return array<string, mixed>
      */
-    public function rules()
+    public function rules(): array
     {
         return [
-            'id' => 'required|integer',
-            'username' => 'required|string'
+            'id' => 'required|integer|exists:images,id',
+        ];
+    }
+
+    /**
+     * Get custom error messages for validator errors.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'id.required' => 'The image ID is required',
+            'id.integer' => 'The image ID must be an integer',
+            'id.exists' => 'The specified image does not exist',
+        ];
+    }
+
+    /**
+     * Get custom attributes for validator errors.
+     *
+     * @return array<string, string>
+     */
+    public function attributes(): array
+    {
+        return [
+            'id' => 'image ID',
         ];
     }
 }
