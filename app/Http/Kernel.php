a98b10e470a5906ec1ef2d2aab0e69cb4a3e6388
Nickolay Smirnov <bzdykin@mail.ru>
2025-05-14 20:43:19 +0300

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,6 +62,7 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'check.friendship' => \App\Http\Middleware\CheckFriendship::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
         'public' => \App\Http\Middleware\PublicUser::class,
