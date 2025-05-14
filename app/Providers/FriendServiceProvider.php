6454d982b626dce7c976c51f536ea8efe9cd6352
Nickolay Smirnov <bzdykin@mail.ru>
2025-05-14 17:29:58 +0300

    Create FriendServiceProvider.php

---
+++ app/Providers/FriendServiceProvider.php
@@ -0,0 +1,19 @@
+// app/Providers/FriendServiceProvider.php
+<?php
+
+namespace App\Providers;
+
+use App\Repositories\FriendRepository;
+use App\Repositories\Interfaces\FriendRepositoryInterface;
+use App\Services\FriendService;
+use App\Services\Interfaces\FriendServiceInterface;
+use Illuminate\Support\ServiceProvider;
+
+class FriendServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        $this->app->bind(FriendServiceInterface::class, FriendService::class);
+        $this->app->bind(FriendRepositoryInterface::class, FriendRepository::class);
+    }
+}
