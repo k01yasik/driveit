5d32b3334bd223cbc57a59f1e4478d65b85eced4
Nickolay Smirnov <bzdykin@mail.ru>
2025-05-14 04:35:27 +0300

    Update FavoriteRepositoryInterface.php

--- app/Repositories/Interfaces/FavoriteRepositoryInterface.php
+++ app/Repositories/Interfaces/FavoriteRepositoryInterface.php
@@ -4,12 +4,13 @@ namespace App\Repositories\Interfaces;
 
 interface FavoriteRepositoryInterface
 {
-
-    public function delete(int $imageId): void;
-
-    public function getFavCountForImage(int $imageId): int;
-
-    public function removeVote(int $userId, int $imageId): void;
-
-    public function add(int $userId, int $imageId): void;
+    public function deleteAllForImage(int $imageId): void;
+    
+    public function getFavoritesCountForImage(int $imageId): int;
+    
+    public function removeFavorite(int $userId, int $imageId): void;
+    
+    public function addFavorite(int $userId, int $imageId): void;
+    
+    public function userHasFavorite(int $userId, int $imageId): bool;
 }
