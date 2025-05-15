06e89af6548665bdf23f1384b86d84ca1a8717a4
Nickolay Smirnov <bzdykin@mail.ru>
2025-05-16 00:30:53 +0300

    Update MessageService.php

--- app/Services/MessageService.php
+++ app/Services/MessageService.php
@@ -4,25 +4,33 @@ namespace App\Services;
 
 use App\Entities\Message;
 use App\Repositories\Interfaces\MessageRepositoryInterface;
+use App\Factories\MessageFactory;
 
 class MessageService
 {
-    public function __construct(
-        private MessageRepositoryInterface $messageRepository
-    ) {}
+    private MessageRepositoryInterface $messageRepository;
+    private MessageFactory $messageFactory;
 
-    public function createMessage(int $userId, int $friendId, string $messageText): Message
-    {
-        return Message::create($userId, $friendId, $messageText);
+    public function __construct(
+        MessageRepositoryInterface $messageRepository,
+        MessageFactory $messageFactory
+    ) {
+        $this->messageRepository = $messageRepository;
+        $this->messageFactory = $messageFactory;
     }
 
-    public function saveMessage(Message $message): void
+    public function addMessage(Message $message): void
     {
         $this->messageRepository->add($message);
     }
 
-    public function getConversation(int $currentId, int $friendId): array
+    public function getConversation(int $currentUserId, int $friendId): array
     {
-        return $this->messageRepository->getMessages($currentId, $friendId);
+        return $this->messageRepository->getMessages($currentUserId, $friendId);
+    }
+
+    public function createMessage(int $userId, int $friendId, string $text): Message
+    {
+        return $this->messageFactory->create($userId, $friendId, $text);
     }
 }
