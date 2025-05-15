d8d7e13683658483911c8760e3cc9685ceb1de2a
Nickolay Smirnov <bzdykin@mail.ru>
2025-05-15 13:53:10 +0300

    Update Image.php

--- app/Image.php
+++ app/Image.php
@@ -2,45 +2,46 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
-/**
- * App\Image
- *
- * @property int $id
- * @property string $url
- * @property string $path
- * @property string $name
- * @property string|null $url_thumbnail
- * @property string|null $path_thumbnail
- * @property int $album_id
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property-read \App\Album $album
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Favorite[] $favorites
- * @property-read int|null $favorites_count
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Image newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Image newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Image query()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Image whereAlbumId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Image whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Image whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Image whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Image wherePath($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Image wherePathThumbnail($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Image whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Image whereUrl($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Image whereUrlThumbnail($value)
- * @mixin \Eloquent
- */
 class Image extends Model
 {
-    public function album()
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<string>
+     */
+    protected $fillable = [
+        'url',
+        'path',
+        'name',
+        'url_thumbnail',
+        'path_thumbnail',
+        'album_id',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    public function album(): BelongsTo
     {
-        return $this->belongsTo('App\Album');
+        return $this->belongsTo(Album::class);
     }
 
-    public function favorites() {
-        return $this->hasMany('App\Favorite');
+    public function favorites(): HasMany
+    {
+        return $this->hasMany(Favorite::class);
     }
 }
