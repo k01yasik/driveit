8dd3e189de2f4d47502bb814b920765ff9ff30a7
Nickolay Smirnov <bzdykin@mail.ru>
2025-05-15 05:53:16 +0300

    Update User.php

--- app/User.php
+++ app/User.php
@@ -2,136 +2,98 @@
 
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
-/**
- * App\User
- *
- * @property int $id
- * @property string $username
- * @property string $email
- * @property string|null $email_verified_at
- * @property string|null $password
- * @property string|null $provider
- * @property string|null $provider_id
- * @property string|null $remember_token
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Album[] $albums
- * @property-read int|null $albums_count
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Comment[] $comments
- * @property-read int|null $comments_count
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Friend[] $friends
- * @property-read int|null $friends_count
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Message[] $messages
- * @property-read int|null $messages_count
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\News[] $news
- * @property-read int|null $news_count
- * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
- * @property-read int|null $notifications_count
- * @property-read \Illuminate\Database\Eloquent\Collection|\Spatie\Permission\Models\Permission[] $permissions
- * @property-read int|null $permissions_count
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Post[] $posts
- * @property-read int|null $posts_count
- * @property-read \App\Profile $profile
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Rating[] $rating
- * @property-read int|null $rating_count
- * @property-read \App\Rip $rip
- * @property-read \Illuminate\Database\Eloquent\Collection|\Spatie\Permission\Models\Role[] $roles
- * @property-read int|null $roles_count
- * @method static \Illuminate\Database\Eloquent\Builder|\App\User newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\User newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\User permission($permissions)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\User query()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\User role($roles, $guard = null)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereEmail($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereEmailVerifiedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\User wherePassword($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereProvider($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereProviderId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereRememberToken($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereUsername($value)
- * @mixin \Eloquent
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Draft[] $drafts
- * @property-read int|null $drafts_count
- * @property-read \Illuminate\Database\Eloquent\Collection|\Laravel\Sanctum\PersonalAccessToken[] $tokens
- * @property-read int|null $tokens_count
- */
 class User extends Authenticatable implements MustVerifyEmail
 {
-    use Notifiable, HasRoles, HasApiTokens;
+    use HasApiTokens, HasFactory, HasRoles, Notifiable;
 
     /**
      * The attributes that are mass assignable.
      *
-     * @var array
+     * @var array<string>
      */
     protected $fillable = [
-        'username', 'email', 'password', 'provider', 'provider_id',
+        'username',
+        'email',
+        'password',
+        'provider',
+        'provider_id',
     ];
 
     /**
-     * The attributes that should be hidden for arrays.
+     * The attributes that should be hidden for serialization.
      *
-     * @var array
+     * @var array<string>
      */
     protected $hidden = [
-        'password', 'remember_token', 'provider', 'provider_id'
+        'password',
+        'remember_token',
+        'provider',
+        'provider_id',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
     ];
 
     public function posts()
     {
-        return $this->hasMany('App\Post');
+        return $this->hasMany(Post::class);
     }
 
-    public function news() {
-        return $this->hasMany('App\News');
+    public function news()
+    {
+        return $this->hasMany(News::class);
     }
 
     public function profile()
     {
-        return $this->hasOne('App\Profile');
+        return $this->hasOne(Profile::class);
     }
 
     public function rip()
     {
-        return $this->hasOne('App\Rip');
+        return $this->hasOne(Rip::class);
     }
 
     public function friends()
     {
-        return $this->hasMany('App\Friend');
+        return $this->hasMany(Friend::class);
     }
 
     public function albums()
     {
-        return $this->hasMany('App\Album');
+        return $this->hasMany(Album::class);
     }
 
     public function comments()
     {
-        return $this->hasMany('App\Comment');
+        return $this->hasMany(Comment::class);
     }
 
     public function rating()
     {
-        return $this->hasMany('App\Rating');
+        return $this->hasMany(Rating::class);
     }
 
     public function messages()
     {
-        return $this->hasMany('App\Message');
+        return $this->hasMany(Message::class);
     }
 
     public function drafts()
     {
-        return $this->hasMany('App\Draft');
+        return $this->hasMany(Draft::class);
     }
 }
