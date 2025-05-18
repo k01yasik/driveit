07fdfa29fcb4cbd8366e3f15031d41d4899c0dc3
Nickolay Smirnov <bzdykin@mail.ru>
2025-05-18 15:46:17 +0300

    Update Comment.php

--- app/Comment.php
+++ app/Comment.php
@@ -2,30 +2,28 @@
 
 declare(strict_types=1);
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Support\Carbon;
 
-/**
- * @property int $id
- * @property int $user_id
- * @property int $post_id
- * @property string $message
- * @property bool $is_verified
- * @property int $level
- * @property int|null $parent_id
- * @property Carbon $created_at
- * @property Carbon $updated_at
- * @property-read User $user
- * @property-read Post $post
- */
 class Comment extends Model
 {
     protected $casts = [
         'is_verified' => 'boolean',
+        'level' => 'integer',
+        'parent_id' => 'integer'
     ];
+
+    public function scopeVerified(Builder $query): Builder
+    {
+        return $query->where('is_verified', true);
+    }
+
+    public function scopeUnverified(Builder $query): Builder
+    {
+        return $query->where('is_verified', false);
+    }
 
     public function user(): BelongsTo
     {
@@ -35,22 +33,5 @@ class Comment extends Model
     public function post(): BelongsTo
     {
         return $this->belongsTo(Post::class);
-    }
-
-    public function scopeVerified($query)
-    {
-        return $query->where('is_verified', true);
-    }
-
-    public function scopeNotVerified($query)
-    {
-        return $query->where('is_verified', false);
-    }
-
-    public function getCreatedAtAttribute($value): string
-    {
-        return Carbon::parse($value)
-            ->locale('ru')
-            ->diffForHumans(null, false, false, 1);
     }
 }
