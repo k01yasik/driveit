ef49f61c090695e58f0d12ccc180753caa6fb7a4
Nickolay Smirnov <bzdykin@mail.ru>
2025-05-18 05:26:39 +0300

    Update CommentController.php

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -21,7 +21,29 @@ class CommentController extends Controller
 
     public function store(Request $request, int $postId): JsonResponse
     {
-        $validated = $request->validate([...]);
+        $validated = $request->validate([
+            'message' => [
+                'required',
+                'string',
+                'max:5000',
+                function ($attribute, $value, $fail) {
+                    if (strip_tags($value) !== $value) {
+                        $fail('The '.$attribute.' contains disallowed HTML tags.');
+                    }
+                }
+            ],
+            'level' => 'required|integer|min:0|max:5',
+            'parent_id' => [
+                'nullable',
+                'integer',
+                'exists:comments,id',
+                function ($attribute, $value, $fail) use ($postId) {
+                    if ($value && !$this->commentService->isParentValid($value, $postId)) {
+                        $fail('Invalid parent comment');
+                    }
+                }
+            ]
+        ]);
 
         $dto = new CommentDTO(
             userId: auth()->id(),
