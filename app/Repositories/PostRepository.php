b4cf7e274eb1918cb456bc852ff8bf1012196eaf
Nickolay Smirnov <bzdykin@mail.ru>
2025-05-20 07:35:57 +0300

    Update PostRepository.php

--- app/Repositories/PostRepository.php
+++ app/Repositories/PostRepository.php
@@ -104,7 +104,7 @@ class PostRepository implements PostRepositoryInterface
         ->where('slug', $slug)
         ->where('is_published', 1)
         ->first(); 
-        dd(post);
+        dd($post);
     
         if (!$post) {
             abort(404);
