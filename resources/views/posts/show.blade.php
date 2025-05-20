e2516dcc04688c11287cf78ba05de088ca7c06a9
Nickolay Smirnov <bzdykin@mail.ru>
2025-05-20 07:19:28 +0300

    Update show.blade.php

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -115,7 +115,7 @@
                             L21.57,2.049z"></path>
                         </g>
                     </svg>
-                    <p>{{ $post['post']['rating_count'] }}</p>
+                    <p>{{ $post['rating_count'] }}</p>
                 </div>
                 <div class="comments-block">
                     <svg version="1.1" class="comments-svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 511.626 511.626" style="enable-background:new 0 0 511.626 511.626;"
