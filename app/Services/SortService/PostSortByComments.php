b54b79372e60a584695e2c11b0a40a806df7b3eb
Nickolay Smirnov <bzdykin@mail.ru>
2025-05-22 23:03:22 +0300

    Update PostSortByComments.php

--- app/Services/SortService/PostSortByComments.php
+++ app/Services/SortService/PostSortByComments.php
@@ -10,13 +10,15 @@ class PostSortByComments implements Sort
 {
     public function sort(array $posts): array
     {
-        $array_column = array_column($posts, 'comments_count');
-
-        array_multisort($array_column, SORT_DESC,
-                            array_column($posts, 'updated_at'), SORT_DESC,
-                            $posts
-        );
-
+        usort($posts, function ($a, $b) {
+        // First sort by comments_count DESC
+            if ($a['comments_count'] != $b['comments_count']) {
+                return $b['comments_count'] <=> $a['comments_count'];
+            }
+            // Then sort by updated_at DESC
+            return $b['updated_at'] <=> $a['updated_at'];
+        });
+    
         return $posts;
     }
 }
