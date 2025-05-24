09884f0d54c084c1f450d894baba2864a64604ac
Nickolay Smirnov <bzdykin@mail.ru>
2025-05-24 03:04:54 +0300

    Update googleanalytics.blade.php

--- resources/views/components/googleanalytics.blade.php
+++ resources/views/components/googleanalytics.blade.php
@@ -2,12 +2,4 @@
     let posts = {!! json_encode($posts, JSON_HEX_TAG) !!};
     let commentsVerified = {!! $commentsVerified !!};
     let commentsNotVerified = {!! $commentsNotVerified !!};
-    {{— let datesQuery = {!! json_encode($datesQuery, JSON_HEX_TAG) !!}; —}}
-    let usersQuery = {!! json_encode($usersQuery, JSON_HEX_TAG) !!};
-    let sessionQuery = {!! json_encode($sessionQuery, JSON_HEX_TAG) !!};
-    let hitsQuery = {!! json_encode($hitsQuery, JSON_HEX_TAG) !!};
-    let countryQueryLabels = {!! json_encode($countryQueryLabels, JSON_HEX_TAG) !!};
-    let countryQueryData = {!! json_encode($countryQueryData, JSON_HEX_TAG) !!};
-    let cityQueryLabels = {!! json_encode($cityQueryLabels, JSON_HEX_TAG) !!};
-    let cityQueryData = {!! json_encode($cityQueryData, JSON_HEX_TAG) !!}
 </script>
