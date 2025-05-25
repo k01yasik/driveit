bd7bd8094db457ff6d37eb0d2a9603d77e7864aa
Nickolay Smirnov <bzdykin@mail.ru>
2025-05-26 01:55:53 +0300

    Update bootstrap.ts

--- resources/assets/js/bootstrap.ts
+++ resources/assets/js/bootstrap.ts
@@ -1,68 +1,74 @@
-window._ = require('lodash');
-//window.Popper = require('popper.js').default;
-
-/**
- * We'll load jQuery and the Bootstrap jQuery plugin which provides support
- * for JavaScript based Bootstrap features such as modals and tabs. This
- * code may be modified to fit the specific needs of your application.
- */
-
-try {
-    window.$ = window.jQuery = require('jquery');
-} catch (e) {}
-
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
-
-window.axios = require('axios');
-
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-window.axios.defaults.withCredentials = true;
-
-/**
- * Next we will register the CSRF Token as a common header with Axios so that
- * all outgoing HTTP requests automatically have it attached. This is just
- * a simple convenience so we don't have to attach every token manually.
- */
-
-let token = document.head.querySelector('meta[name="csrf-token"]');
-
-if (token) {
-    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
-} else {
-    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
-}
-
-
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
-
-import Echo from "laravel-echo";
+// Import dependencies directly instead of attaching to window where possible
+import _ from 'lodash';
+import axios from 'axios';
+import jQuery from 'jquery';
+import Echo from 'laravel-echo';
+import io from 'socket.io-client';
+import smartcrop from 'smartcrop';
 import Chart from 'chart.js';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
-import tippy, {roundArrow} from 'tippy.js';
+import tippy, { roundArrow } from 'tippy.js';
 import 'tippy.js/dist/tippy.css';
 import 'tippy.js/dist/svg-arrow.css';
 import 'tippy.js/themes/material.css';
 
-tippy('.tippy', {
-    theme: 'material',
-    arrow: roundArrow,
-    duration: 500,
-    placement: 'right'
-});
+// Type declarations for window extensions
+declare global {
+  interface Window {
+    $: typeof jQuery;
+    jQuery: typeof jQuery;
+    axios: typeof axios;
+    Echo: Echo;
+    io: typeof io;
+    smartcrop: typeof smartcrop;
+    Chart: typeof Chart;
+  }
+}
 
-window.io = require('socket.io-client');
-window.smartcrop = require('smartcrop');
+// Initialize jQuery
+try {
+  window.$ = window.jQuery = jQuery;
+} catch (e) {
+  console.error('jQuery initialization failed', e);
+}
 
+// Initialize Lodash
+window._ = _;
+
+// Configure Axios
+window.axios = axios;
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.withCredentials = true;
+
+// Set CSRF token
+const token = document.head.querySelector('meta[name="csrf-token"]');
+if (token) {
+  window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.getAttribute('content');
+} else {
+  console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
+
+// Initialize Socket.io and Echo
+window.io = io;
 window.Echo = new Echo({
-    broadcaster: 'socket.io',
-    host: window.location.hostname //+ ':6001'
+  broadcaster: 'socket.io',
+  host: window.location.hostname //+ ':6001'
 });
+
+// Initialize Smartcrop
+window.smartcrop = smartcrop;
+
+// Initialize Chart.js
+window.Chart = Chart;
+Chart.plugins.register(ChartDataLabels);
+
+// Configure Tippy.js
+tippy('.tippy', {
+  theme: 'material',
+  arrow: roundArrow,
+  duration: 500,
+  placement: 'right'
+});
+
+// Optional: Export initialized libraries for module usage
+export { axios, jQuery as $, _, Echo, io, smartcrop, Chart, tippy };
