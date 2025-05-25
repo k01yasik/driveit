4bb36642e1f1e374e0532195f4f0225d733dbce5
Nickolay Smirnov <bzdykin@mail.ru>
2025-05-26 02:27:41 +0300

    Update bootstrap.ts

--- resources/assets/js/bootstrap.ts
+++ resources/assets/js/bootstrap.ts
@@ -1,18 +1,18 @@
-// Import dependencies directly instead of attaching to window where possible
+// Import dependencies
 import _ from 'lodash';
 import axios from 'axios';
 import jQuery from 'jquery';
 import Echo from 'laravel-echo';
 import io from 'socket.io-client';
 import smartcrop from 'smartcrop';
-import Chart from 'chart.js';
+import { Chart, registerables } from 'chart.js';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
 import tippy, { roundArrow } from 'tippy.js';
 import 'tippy.js/dist/tippy.css';
 import 'tippy.js/dist/svg-arrow.css';
 import 'tippy.js/themes/material.css';
 
-// Type declarations for window extensions
+// Type declarations
 declare global {
   interface Window {
     $: typeof jQuery;
@@ -25,18 +25,14 @@ declare global {
   }
 }
 
-// Initialize jQuery
-try {
-  window.$ = window.jQuery = jQuery;
-} catch (e) {
-  console.error('jQuery initialization failed', e);
-}
-
-// Initialize Lodash
+// Initialize libraries
 window._ = _;
+window.$ = window.jQuery = jQuery;
+window.axios = axios;
+window.io = io;
+window.smartcrop = smartcrop;
 
 // Configure Axios
-window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.axios.defaults.withCredentials = true;
 
@@ -44,23 +40,17 @@ window.axios.defaults.withCredentials = true;
 const token = document.head.querySelector('meta[name="csrf-token"]');
 if (token) {
   window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.getAttribute('content');
-} else {
-  console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
 
-// Initialize Socket.io and Echo
-window.io = io;
+// Initialize Chart.js
+Chart.register(...registerables, ChartDataLabels);
+window.Chart = Chart;
+
+// Initialize Echo
 window.Echo = new Echo({
   broadcaster: 'socket.io',
-  host: window.location.hostname //+ ':6001'
+  host: window.location.hostname
 });
-
-// Initialize Smartcrop
-window.smartcrop = smartcrop;
-
-// Initialize Chart.js
-window.Chart = Chart;
-Chart.plugins.register(ChartDataLabels);
 
 // Configure Tippy.js
 tippy('.tippy', {
@@ -69,6 +59,3 @@ tippy('.tippy', {
   duration: 500,
   placement: 'right'
 });
-
-// Optional: Export initialized libraries for module usage
-export { axios, jQuery as $, _, Echo, io, smartcrop, Chart, tippy };
