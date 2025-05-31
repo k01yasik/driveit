1e6290ed7aecbfc801d3ede1753035deff7865e2
Nickolay Smirnov <bzdykin@mail.ru>
2025-05-31 03:09:56 +0300

    Update NewsController.php

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -3,37 +3,77 @@
 namespace App\Http\Controllers;
 
 use App\Services\NewsService;
-use Illuminate\Http\Request;
+use App\Http\Requests\NewsRequest;
+use Illuminate\Http\JsonResponse;
+use Illuminate\View\View;
+use Symfony\Component\HttpFoundation\Response;
 
 class NewsController extends Controller
 {
-    protected NewsService $newsService;
-
-    public function __construct(NewsService $newsService)
-    {
-        $this->newsService = $newsService;
+    public function __construct(
+        private NewsService $newsService
+    ) {
     }
 
-
-    public function index(Request $request) {
+    public function index(): View
+    {
         $news = $this->newsService->getLastNews();
-
         return view('news.index', compact('news'));
     }
 
-    public function store(Request $request) {
-
+    public function list(): View
+    {
+        $news = $this->newsService->getPaginatedNews();
+        return view('news.list', compact('news'));
     }
 
-    public function update(Request $request, $id) {
-
+    public function store(NewsRequest $request): JsonResponse
+    {
+        try {
+            $data = $request->validated();
+            $news = $this->newsService->create($data);
+            return response()->json($news, Response::HTTP_CREATED);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Failed to create news'
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
-    public function delete(Request $request, $id) {
-
+    public function update(NewsRequest $request, int $id): JsonResponse
+    {
+        try {
+            $data = $request->validated();
+            $success = $this->newsService->update($id, $data);
+            return response()->json(['success' => $success]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Failed to update news'
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
-    public function publishToggle(Request $request, $id) {
+    public function delete(int $id): JsonResponse
+    {
+        try {
+            $success = $this->newsService->delete($id);
+            return response()->json(['success' => $success]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Failed to delete news'
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
 
+    public function publishToggle(int $id): JsonResponse
+    {
+        try {
+            $success = $this->newsService->togglePublish($id);
+            return response()->json(['success' => $success]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Failed to toggle publish status'
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 }
