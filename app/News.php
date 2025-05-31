d3d3ee2d511dc3f7075672f4a4f5c9bfc88c15d5
Nickolay Smirnov <bzdykin@mail.ru>
2025-05-31 03:11:31 +0300

    Update News.php

--- app/News.php
+++ app/News.php
@@ -4,58 +4,49 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Traits\Publishable;
 
-/**
- * App\News
- *
- * @property int $id
- * @property string $slug
- * @property string $title
- * @property string $description
- * @property string $name
- * @property string $caption
- * @property string $body
- * @property string $image_path
- * @property int $is_published
- * @property string|null $date_published
- * @property int|null $user_id
- * @property int $views
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property-read \App\User|null $user
- * @method static Builder|News newModelQuery()
- * @method static Builder|News newQuery()
- * @method static Builder|News query()
- * @method static Builder|News whereBody($value)
- * @method static Builder|News whereCaption($value)
- * @method static Builder|News whereCreatedAt($value)
- * @method static Builder|News whereDatePublished($value)
- * @method static Builder|News whereDescription($value)
- * @method static Builder|News whereId($value)
- * @method static Builder|News whereImagePath($value)
- * @method static Builder|News whereIsPublished($value)
- * @method static Builder|News whereName($value)
- * @method static Builder|News whereSlug($value)
- * @method static Builder|News whereTitle($value)
- * @method static Builder|News whereUpdatedAt($value)
- * @method static Builder|News whereUserId($value)
- * @method static Builder|News whereViews($value)
- * @mixin \Eloquent
- * @method static Builder|\News published()
- */
 class News extends Model
 {
-    public function user() {
-        return $this->belongsTo('App\User');
+    use SoftDeletes, Publishable;
+
+    public const STATUS_PUBLISHED = 1;
+    public const STATUS_DRAFT = 0;
+
+    protected $fillable = [
+        'slug',
+        'title',
+        'description',
+        'name',
+        'caption',
+        'body',
+        'image_path',
+        'is_published',
+        'date_published',
+        'user_id',
+        'views'
+    ];
+
+    protected $casts = [
+        'is_published' => 'boolean',
+        'date_published' => 'datetime',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 
-
-    /**
-     * @param Builder $query
-     * @return Builder;
-     */
-    public function scopePublished($query)
+    public function scopePublished(Builder $query): Builder
     {
-        return $query->where('is_published', true);
+        return $query->where('is_published', self::STATUS_PUBLISHED)
+            ->whereNotNull('date_published')
+            ->where('date_published', '<=', now());
+    }
+
+    public function incrementViews(): void
+    {
+        $this->increment('views');
     }
 }
