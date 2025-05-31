52f146dee39d454ea27b8b8dde1ff25961395dda
Nickolay Smirnov <bzdykin@mail.ru>
2025-05-31 03:07:59 +0300

    Update NewsRepositoryInterface.php

--- app/Repositories/Interfaces/NewsRepositoryInterface.php
+++ app/Repositories/Interfaces/NewsRepositoryInterface.php
@@ -2,8 +2,16 @@
 
 namespace App\Repositories\Interfaces;
 
+use App\Dto\NewsCollection;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 
 interface NewsRepositoryInterface
 {
-    public function getLastNews(): array;
+    public function getLastNews(int $limit = 10): NewsCollection;
+    public function getPaginatedNews(int $perPage = 15): LengthAwarePaginator;
+    public function findById(int $id): ?array;
+    public function create(array $data): array;
+    public function update(int $id, array $data): bool;
+    public function delete(int $id): bool;
+    public function togglePublish(int $id): bool;
 }
