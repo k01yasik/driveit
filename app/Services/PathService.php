278e2fbdeda7ccf92e6170ca5915ba1edcc511dc
Nickolay Smirnov <bzdykin@mail.ru>
2025-06-09 01:42:04 +0300

    Update PathService.php

--- app/Services/PathService.php
+++ app/Services/PathService.php
@@ -2,14 +2,44 @@
 
 namespace App\Services;
 
-
 class PathService
 {
-    public function createThumbnailPath(string $username, string $albumPath, string $imageName) {
-        return $username.'/'.$albumPath.'/thumbnail/'.$imageName;
+    private const THUMBNAIL_SUBPATH = 'thumbnail';
+    private const AVATARS_SUBPATH = 'avatars';
+    
+    /**
+     * Create a thumbnail path using structured components
+     *
+     * @param string $username
+     * @param string $albumPath
+     * @param string $imageName
+     * @return string
+     */
+    public function createThumbnailPath(string $username, string $albumPath, string $imageName): string
+    {
+        return $this->buildPath([$username, $albumPath, self::THUMBNAIL_SUBPATH, $imageName]);
     }
 
-    public function createAvatarPath(string $username, string $imageName){
-        return $username.'/avatars/'.$imageName;
+    /**
+     * Create an avatar path using structured components
+     *
+     * @param string $username
+     * @param string $imageName
+     * @return string
+     */
+    public function createAvatarPath(string $username, string $imageName): string
+    {
+        return $this->buildPath([$username, self::AVATARS_SUBPATH, $imageName]);
+    }
+
+    /**
+     * Build path from components
+     *
+     * @param array $pathComponents
+     * @return string
+     */
+    protected function buildPath(array $pathComponents): string
+    {
+        return implode('/', array_filter($pathComponents));
     }
 }
